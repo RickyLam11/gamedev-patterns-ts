5c68d3c159b09129ccccc50099267ce23ea5b2c3
Ricky From Hong Kong <lamricky11@hotmail.com>
2023-02-05 18:03:09 +0100

    test: make grid/onclick test Setting-prove

--- src/grid/components/onclick/onclick.spec.ts
+++ src/grid/components/onclick/onclick.spec.ts
@@ -1,6 +1,7 @@
 import { GridOnClickComponent } from './onclick'
 import { mockGridFactory } from '@/grid'
 import { Vector2D } from '@/utils'
+import { Settings } from '@/settings'
 
 describe('>>> Grid Click Component', () => {
   let comp: GridOnClickComponent
@@ -13,8 +14,9 @@ describe('>>> Grid Click Component', () => {
 
   it('should update node if user click with it\'s range', () => {
     const spy = jest.spyOn(comp.Entity, 'DeterminePathTo')
+    const centerOfNode = new Vector2D(Settings.grid.nodeSize / 2, Settings.grid.nodeSize / 2)
 
-    comp.ClickOn(new Vector2D(100, 100))
+    comp.ClickOn(centerOfNode)
     expect(spy).toBeCalledWith(comp.Entity.Nodes[0])
   })
 })
