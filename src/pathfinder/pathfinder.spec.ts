8ddfb0961a19fc021cbb157ab678e036d6dc7583
Ricky From Hong Kong <lamricky11@hotmail.com>
2023-02-05 17:20:45 +0100

    refactor: follow coordinate system in grid

--- src/pathfinder/pathfinder.spec.ts
+++ src/pathfinder/pathfinder.spec.ts
@@ -35,32 +35,32 @@ describe('>>> Pathfinder', () => {
   }
 
   // define instance
-  // first row
+  // first row, y = 0
   const node00 = new GraphNode(new Vector2D(0, 0))
-  const node01 = new GraphNode(new Vector2D(0, 1))
-  const node02 = new GraphNode(new Vector2D(0, 2))
-  // second row
   const node10 = new GraphNode(new Vector2D(1, 0))
-  const node11 = new GraphNode(new Vector2D(1, 1))
-  const node12 = new GraphNode(new Vector2D(1, 2))
-  // third row
   const node20 = new GraphNode(new Vector2D(2, 0))
+  // second row, y = 1
+  const node01 = new GraphNode(new Vector2D(0, 1))
+  const node11 = new GraphNode(new Vector2D(1, 1))
   const node21 = new GraphNode(new Vector2D(2, 1))
+  // third row, y = 2
+  const node02 = new GraphNode(new Vector2D(0, 2))
+  const node12 = new GraphNode(new Vector2D(1, 2))
   const node22 = new GraphNode(new Vector2D(2, 2))
 
   const edges = {
     // in T,R,B,L order
-    [node00.Position.AsString()]: [node01, node10],
-    [node01.Position.AsString()]: [node02, node11, node00],
-    [node02.Position.AsString()]: [node12, node01],
+    [node00.Position.AsString()]: [node10, node01],
+    [node10.Position.AsString()]: [node20, node11, node00],
+    [node20.Position.AsString()]: [node21, node10],
 
-    [node10.Position.AsString()]: [node00, node11, node20],
-    [node11.Position.AsString()]: [node01, node12, node21, node10],
-    [node12.Position.AsString()]: [node02, node22, node11],
+    [node01.Position.AsString()]: [node00, node11, node02],
+    [node11.Position.AsString()]: [node10, node21, node12, node01],
+    [node21.Position.AsString()]: [node20, node22, node11],
 
-    [node20.Position.AsString()]: [node10, node21],
-    [node21.Position.AsString()]: [node11, node22, node20],
-    [node22.Position.AsString()]: [node12, node21],
+    [node02.Position.AsString()]: [node01, node12],
+    [node12.Position.AsString()]: [node11, node22, node02],
+    [node22.Position.AsString()]: [node21, node12],
   }
 
   const heuristic = (a: IGraphNode, b: IGraphNode): number => Math.abs(a.Position.x - b.Position.x) + Math.abs(a.Position.y - b.Position.y)
@@ -69,19 +69,19 @@ describe('>>> Pathfinder', () => {
 
   it('should calculate path from point A to point B', () => {
     expect(pathfinder.CalculatePath(node00, node22)).toStrictEqual([
-      node01, node11, node12, node22
+      node10, node11, node21, node22
     ])
-    expect(pathfinder.CalculatePath(node01, node12)).toStrictEqual([
-      node11, node12
+    expect(pathfinder.CalculatePath(node01, node21)).toStrictEqual([
+      node11, node21
     ])
-    expect(pathfinder.CalculatePath(node20, node02)).toStrictEqual([
-      node10, node11, node01, node02
+    expect(pathfinder.CalculatePath(node02, node20)).toStrictEqual([
+      node01, node11, node10, node20
     ])
-    expect(pathfinder.CalculatePath(node12, node20)).toStrictEqual([
-      node11, node21, node20
+    expect(pathfinder.CalculatePath(node21, node02)).toStrictEqual([
+      node11, node12, node02
     ])
     expect(pathfinder.CalculatePath(node22, node11)).toStrictEqual([
-      node12, node11
+      node21, node11
     ])
   })
 })
